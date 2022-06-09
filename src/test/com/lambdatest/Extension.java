fa6031c9cbd40ca373f9f92730e9d3c5cf9f1140
Sirajuddin Khan <sirajuddinkhan88@gmail.com>
2022-06-09 20:42:34 +0000

    extension testing selenium java example

--- src/test/com/lambdatest/Extension.java
+++ src/test/com/lambdatest/Extension.java
@@ -1,5 +1,6 @@
 package com.lambdatest;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Set;
@@ -7,15 +8,16 @@ import java.util.Set;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-public class ElementByText {
+public class Extension {
 
     private RemoteWebDriver driver;
     private String Status = "failed";
 
-   
+
     public void setup() throws MalformedURLException {
         String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
         String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
@@ -33,34 +35,19 @@ public class ElementByText {
         String[] Tags = new String[] { "Feature", "Falcon", "Severe" };
         caps.setCapability("tags", Tags);
 
+        // Extension upload here using chrome options
+        ChromeOptions options = new ChromeOptions ();
+        options.addExtensions (new File("./LambdatestScreenshotExtension.crx"));
+        caps.setCapability(ChromeOptions.CAPABILITY, options);
+
         driver = new RemoteWebDriver(new URL("https://" + username + ":" + authkey + hub), caps);
 
     }
-
-   
-    public void elementByTextTest() throws InterruptedException {
+    public void extensionTest() throws InterruptedException {
         String spanText;
         System.out.println("Loading Url");
 
         driver.get("https://lambdatest.github.io/sample-todo-app/");
-        /*
-         * text(): A built-in method in Selenium WebDriver that is used with XPath
-         * locator to locate an element based on its exact text value.
-         * Example: //*[ text() = ‘5 of 5 remaining’ ]
-         * contains(): Similar to the text() method, contains() is another built-in
-         * method used to locate an element based on partial text match.
-         * For example, if we need to locate a label that has “5 of 5 remaining” as its
-         * text, it can be located using the following line of code with Xpath.
-         * Example: //*[ contains (text(), ‘5 of 5’ ) ]
-         */
-
-        // Locating element with text()
-        WebElement e = driver.findElement(By.xpath("//*[text()='5 of 5 remaining']"));
-        System.out.println(e.getText());
-
-        // located element with contains()
-        WebElement m = driver.findElement(By.xpath("//*[contains(text(),'5 of 5')]"));
-        System.out.println(m.getText());
 
         System.out.println("Checking Box");
         driver.findElement(By.name("li1")).click();
@@ -105,9 +92,6 @@ public class ElementByText {
         driver.findElement(By.name("li9")).click();
 
         // Let's also assert that the todo we added is present in the list.
-
-        spanText = driver.findElement(By.xpath("/html/body/div/div/div/ul/li[9]/span")).getText();
-        "Get Taste of Lambda and Stick to It".equalsIgnoreCase(spanText);
         Status = "passed";
         Thread.sleep(150);
 
@@ -115,10 +99,12 @@ public class ElementByText {
 
     }
 
+ 
     public void tearDown() {
         driver.executeScript("lambda-status=" + Status);
         driver.quit();
     }
+
 
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
         ElementByText test = new ElementByText();
